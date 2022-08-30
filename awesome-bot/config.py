e829b46b7f6aa7903fb881c97a1a67449301993e
wujyi <100174735+wujyi@users.noreply.github.com>
2022-08-30 10:01:14 +0800

    Update config.py

--- awesome-bot/config.py
+++ awesome-bot/config.py
@@ -6,7 +6,7 @@ COMMAND_START = {'', re.compile(r'[/!]+')}
 HOST = '127.0.0.1'
 PORT = 8080
 NICKNAME = {'小南', '囡囡'}
-TENCENT_BOT_SECRET_ID = '腾讯云账号'
-TENCENT_BOT_SECRET_KEY = '腾讯云key'
+TENCENT_BOT_SECRET_ID = '此处为你的腾讯云账号'
+TENCENT_BOT_SECRET_KEY = '此处为你的腾讯云key'
 API_ROOT = 'http://127.0.0.1:8080'
 
